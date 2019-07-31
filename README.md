9059ceabf208dcc9574369bee66864efaf07906c
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2019-07-30 23:55:49 -0400

    message

---
+++ README.md
@@ -0,0 +1,67 @@
+# Credit Card Validator
+
+<!-- 
+Finally, in your readme file, explain why (100-500 words) you preferred to implement the luhn algorithm with javascript for parsing user input as opposed to another language such as scheme. 
+In particular, address how the JavaScript syntax has some weaknesses with concrete examples and 
+describe some ways that people/orgs have tried to overcome the weaknesses?
+ -->
+This project is the second assignment for the C.S. class, CISC.3140. This assignment uses mainly vanilla javascript to perform Luhn's Algorithm in order to verify credit card numbers. 
+
+I chose javascript over scheme because of it's revelance to my interests as a Web Developer. I haven't had much practice coding in vanillaJS which made this assignment all the more fun, and frustrating. I feel a lot more comfortable using javascript's plethora of options when it comes to parsing in user input. This can often be a weakness as there are many functions available to perform one task, some being marginally better than others with how they interact with your code (e.g. textContent vs innerHTML) or simply the speed (e.g. getElementById vs querySelector).
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+1. Install python
+
+```
+brew install python
+```
+
+2. Install Flask using pip3
+
+```
+pip3 install flask
+```
+
+
+### Installing
+
+A step by step series of examples that tell you how to get a development environment running on MacOS:
+
+1. Clone the repo
+
+```
+git clone https://github.com/abrahym-sharfeldden/cc-validator
+```
+
+2. Navigate to the folder and run the local host
+
+```
+cd [~/ToClone/Location]
+```
+
+```
+export FLASK_APP=app.py
+```
+
+```
+flask run
+```
+
+## Built With
+
+* [HTML/CSS]() - User Interface
+* [Javascript]() - Scripting Language
+* [Flask](http://flask.pocoo.org/) - The web framework used
+* [Python](https://www.python.org/) - Programming Language
+
+## Authors
+
+* **Abrahym Sharf Eldden** - *Initial Work* - [abrahym-sharfeldden](https://github.com/abrahym-sharfeldden)
+* **Billie Thompson** - *[README.md Template](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)* - [PurpleBooth](https://github.com/PurpleBooth)
---
+++ static/script.js
@@ -0,0 +1,98 @@
+let checkVal = function(e) {
+	// Only numbers can be entered into the input field.
+	let keyCode = (e.which) ? e.which : e.keyCode;
+
+	return ((keyCode != 46 && keyCode > 31 && (keyCode < 48 || keyCode > 57)) ? false : true);
+}
+
+luhnCheck = function(cardNumber){
+	const alternates = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+	
+	return (
+		cardNumber
+			.toString()
+			.replace(/\s/g, '')
+			.split("")
+			.reverse()
+			.reduce( function(total, current, i) {
+				let num = Number(current);
+				let isOdd = i % 2 == 0;
+
+	    		return total + (isOdd ? num : alternates[num]);
+			}, 0) % 10 == 0
+	);
+}
+
+let submitVal = function() {
+	let log = document.querySelector("p");
+	let cardNumber = document.querySelector('input#cardType').value;
+
+	if(luhnCheck(cardNumber))
+		return log.innerHTML = `Card Verified!`;
+	else
+		return log.innerHTML = `Invalid Card Number!`
+}
+
+let numFormat = function(v){
+	let log = document.querySelector("p");
+	let cardType = document.querySelector('input#cardType');
+  	let visa = checkCardType(v) == "Visa";
+  	let mastercard = checkCardType(v) == "MasterCard";
+  	let amex = checkCardType(v) == "Amex";
+  	let discover = checkCardType(v) == "Discover";
+	
+	document.getElementById("cc-icon").classList = checkCardType(v) ? `fab fa-3x fa-cc-${checkCardType(v).toLowerCase()}` : "fab fa-3x fa-cc-none";
+
+    if(cardType == "")
+    	log.innerHTML = '<br />';
+ 
+    if(amex){
+		cardType.maxLength = "17";
+    	
+    	if (v.match(/^\d{4}$/) !== null) {
+	      	cardType.value = v + ' ';
+	    } 
+	    else if (v.match(/^\d{4}\s\d{6}$/) !== null) {
+	      	cardType.value = v + ' ';
+	    } 
+	    else if(v.match(/^(\d{4}\s\d{6}\s\d{6})$/) !== null) {
+	    	//last grouping of regex set to 6 to prevent 
+	    	// an unnecessary added space.
+	      	cardType.value = v + ' ';
+	    }
+    }
+    else {
+    	// (visa || mastercard || discover)
+		cardType.maxLength = "19";
+    	
+    	if (v.match(/^\d{4}$/) !== null) {
+	      	cardType.value = v + ' ';
+	    } 
+	    else if (v.match(/^\d{4}\s\d{4}$/) !== null) {
+	      	cardType.value = v + ' ';
+	    } 
+	    else if(v.match(/^(\d{4}\s\d{4}\s\d{4})$/) !== null) {
+	      	cardType.value = v + ' ';
+	    }
+    }
+}
+
+let checkCardType = function(cardNumber){	
+	function isAmex() {
+		return (/^3[47]{1,2}/g.test(cardNumber) ? "Amex" : null);
+	}
+	function isVisa() {
+		return (/^4[0-9]{1}/g.test(cardNumber) ? "Visa" : null);
+	}
+	function isMasterCard() {
+		let bool = /^5[1-5]{2}/g.test(cardNumber);
+
+		return (bool ? "MasterCard" : null);
+	}	
+		function isDiscover(){
+			let bool = /^6(?:011\d{12}|5\d{14}|4[4-9]\d{13}|22(?:1(?:2[6-9]|[3-9]\d)|[2-8]\d{2}|9(?:[01]\d|2[0-5]))\d{10})/g
+			return (bool ? "Discover" : null)
+	}
+	
+	return isAmex() || isVisa() || isMasterCard() || isDiscover();
+}
